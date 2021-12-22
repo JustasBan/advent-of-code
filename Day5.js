b10ee938a8c99b734ba63eb3f60ce8738fd1c94b
Justasban <justas.ban2001@gmail.com>
2021-12-22 20:04:18 +0200

    5th day task 1

--- Day5.js
+++ Day5.js
@@ -12,24 +12,93 @@ try {
     const lines = data.split(/\r?\n/)
 
     // put all lines as int to array
-    lines.forEach((line) => {
+    lines.map((line) => {
 
-        let temp = line.split(' -> ')
-
-        input.push(temp)
+        input.push(line.split(' -> '))
+        
     })
 } catch (err) {
     console.error(err)
 }
+//finding maximum length of area in this hell of an object
+let max_len = -99999999999
+
+input.map(x =>{
+    
+    let nums = x.map(y => y.split(',').map(Number))
+
+    nums.map(z => {
+        let maxii = Math.max(...z)
+
+        if(max_len < maxii){
+        max_len = maxii
+
+        }
+    })
+} )
+
+//defining area
+let area = new Array(max_len+1).fill(0);
+
+for(var i = 0; i< area.length; i++){
+    area[i] = new Array(max_len+1).fill(0)
+}
+
+console.log(max_len);
 
 //--------------------------------------------------------------
 //driver code
 
-let area = new Array(10).fill(0);
+let fillArr = (start, end) => {
 
-for(var i = 0; i< area.length; i++){
-    area[i] = new Array(10).fill(0)
-    console.log(area[i])
+    if(start[0] == end[0]){
+        fillVert(start[1], end[1], start[0])
+        
+    }
+    else{
+
+        if(start[1] == end[1]){
+            fillHoriz(start[0], end[0], start[1])
+        }
+    }
+}
+
+//filling horizontal:
+let fillHoriz = (start, end, line) => {
+
+    if(start<end){
+        loopHelperHoriz(start, end, line)
+    }
+    else{
+        loopHelperHoriz(end, start, line)
+    }
+}
+
+let loopHelperHoriz = (first, final, line) => {
+
+    for (let index = first; index <= final; index++) {
+        
+        area[line][index]++
+    }
+}
+
+//filling vertical:
+let fillVert = (start, end, col) => {
+
+    if(start<end){
+        loopHelperVert(start, end, col)
+    }
+    else{
+        loopHelperVert(end, start, col)
+    }
+}
+
+let loopHelperVert = (first, final, col) => {
+    
+    for (let index = first; index <= final; index++) {
+        
+        area[index][col]++
+    }
 }
 
 input.forEach((line) =>{
@@ -37,19 +106,25 @@ input.forEach((line) =>{
     //find positions number values
     let startPos = line[0].split(',').map(Number)
     let endPos = line[1].split(',').map(Number)
-
-    
+    fillArr(startPos, endPos)
 })
 
-let fillArr = (arr, start, end) => {
+let count = 0
 
-    
-}
+area.map(x => {
+    x.map(x => {
+        if(2 <= x){
+            count++
+        }
+    })
+})
+
+console.log(count);
 
 //-----------------------------------------------------------------
 //output:
 
-const data = JSON.stringify(input)
+const data = JSON.stringify(area.filter(x => x.filter(y => y!=0)))
 
 // write file to disk
 fs.writeFile('./out.json', data, 'utf8', (err) => {
